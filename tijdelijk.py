d16a671dc3e15ca12c64bfab3fa59abeb8739879
Malou <malouhessel@hotmail.com>
2023-07-31 11:11:45 +0200

    bestand helper.py toegevoegd

---
+++ tijdelijk.py
@@ -0,0 +1,9 @@
+prijzen = {
+    "aardbei": 3,
+    "vanille": 4,
+    "chocolade": 5
+}
+a = prijzen["aardbei"]
+b = 0.8
+aanbieding = "a * b"
+print(f"Vandaag in de aanbieding: aardbeien-ijs, 1 liter - slechts €{aanbieding}")
